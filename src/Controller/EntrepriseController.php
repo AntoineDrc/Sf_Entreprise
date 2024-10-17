319042df900a04fb780c55323055a3c473c52957
Antoine Delubriac <Antoine.DELUBRIAC@stagiaire-elan.fr>
2024-10-17 11:23:52 +0200

    Création EmployeController + vues "employe", "entreprise"

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Entreprise;
+use App\Repository\EntrepriseRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +12,14 @@ use Symfony\Component\Routing\Attribute\Route;
 class EntrepriseController extends AbstractController
 {
     #[Route('/entreprise', name: 'app_entreprise')]
-    public function index(): Response
+    // public function index(EntityManagerInterface $entityManager): Response
+    public function index(EntrepriseRepository $entrepriseRepository): Response
     {
+        // $entreprises = $entityManager->getRepository(Entreprise::class)->findAll();
+        // $entreprises = $entrepriseRepository->findAll();
+        $entreprises = $entrepriseRepository->findBy(["ville" => "STRASBOURG"], ["raisonSociale" => "ASC"]);
         return $this->render('entreprise/index.html.twig', [
-            'controller_name' => 'EntrepriseController',
+            'entreprises' => $entreprises
         ]);
     }
 }
