1d7fd27a95bbb321047e9b551a32af7d1bb90158
Antoine Delubriac <Antoine.DELUBRIAC@stagiaire-elan.fr>
2024-10-17 15:55:25 +0200

    Ajout Form Entreprise + Employe

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -3,11 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Entreprise;
+use App\Form\EntrepriseType;
 use App\Repository\EntrepriseRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class EntrepriseController extends AbstractController
 {
@@ -23,6 +25,17 @@ class EntrepriseController extends AbstractController
         ]);
     }
 
+    
+    #[Route('/entreprise/new', name: 'new_entreprise')]
+    public function new(Request $request): Response
+    {
+        $entreprise = new Entreprise();
+        
+        $form = $this->createForm(EntrepriseType::class, $entreprise);
+        
+        return $this->render('entreprise/new.html.twig', ['formAddEntreprise' => $form,]);
+    }
+    
     #[Route('/entreprise/{id}', name: 'show_entreprise')]
     public function show(Entreprise $entreprise): Response
     {
